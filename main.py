f8b1250762a5909297f66afed664134dbab7d59b
Nelson <lunanelson287@icloud.com>
2024-07-12 19:24:23 -0700

    first try

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
 
 def getInput():
     """
-    ########################################
-    Code Your Program here
-    ########################################
+    Prompt the user to enter multiple values in a single line,
+    split these values, convert them into a list of integers, and return it.
     """
-
+    user_input = input("Enter multiple values separated by spaces: ")
+    return list(map(int, user_input.split()))
 
 def makeReverse(numbers):
     """
-    ########################################
-    Code Your Program here
-    ########################################
+    Takes a list of numbers as input and returns a new list with the numbers in reverse order.
     """
-
+    reversed_numbers = []
+    while numbers:
+        reversed_numbers.append(numbers.pop())
+    return reversed_numbers
 
 def main():
     numbers = getInput()
     ret = makeReverse(numbers)
     print(f'The result values are: {ret}')
-
 
 if __name__ == '__main__':
     main()
